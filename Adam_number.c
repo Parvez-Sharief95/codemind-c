213ac10641bcfd1a28066c5c5ce6b32a221347e6
Parvez-Sharief95 <119729990+Parvez-Sharief95@users.noreply.github.com>
2022-12-21 21:43:52 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,s,sq,r1,r2,s1=0,s2=0;     scanf("%d",&n);     s=n*n;     while(s!=0)     {         r1=s%10;         s/=10;         s1=s1*10+r1;     }     sq=sqrt(s1);     while(sq!=0)     {         r2=sq%10;         sq/=10;         s2=s2*10+r2;     }     if(s2==n)     {         printf("True");     }     else     {         printf("False");     } }

--- Adam_number.c
+++ Adam_number.c
@@ -2,20 +2,20 @@
 #include<math.h>
 int main()
 {
-    int n,t,r1,r2,s1=0,s2=0,sqr;
+    int n,s,sq,r1,r2,s1=0,s2=0;
     scanf("%d",&n);
-    t=n*n;
-    while(t!=0)
+    s=n*n;
+    while(s!=0)
     {
-        r1=t%10;
-        t/=10;
+        r1=s%10;
+        s/=10;
         s1=s1*10+r1;
     }
-    sqr=sqrt(s1);
-    while(sqr!=0)
+    sq=sqrt(s1);
+    while(sq!=0)
     {
-        r2=sqr%10;
-        sqr/=10;
+        r2=sq%10;
+        sq/=10;
         s2=s2*10+r2;
     }
     if(s2==n)
