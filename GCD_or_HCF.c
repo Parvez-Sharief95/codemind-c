954e7da91d086a49663d0007ee59d329969f886c
Parvez-Sharief95 <119729990+Parvez-Sharief95@users.noreply.github.com>
2023-05-06 11:05:31 +0530

    #include<stdio.h> int main() {     int n,i,m,s=0;     scanf("%d%d",&n,&m);     for(i=1;i<=n&&i<=m;i++)     {         if(n%i==0 && m%i==0)         {             s=i;         }     }     printf("%d",s); }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,24 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int i,a,b,x,y,g;
-	scanf("%d%d",&a,&b);
-	if(a>b)
-	{
-		x=a;
-		y=b;
-	}
-	else
-	{
-		x=b;
-		y=a;
-	}
-	for(i=1;i<=y;i++)
-	{
-		if(x%i==0 && y%i==0)
-		{
-			g=i;
-		}
-	}
-	printf("%d",g);
+    int n,i,m,s=0;
+    scanf("%d%d",&n,&m);
+    for(i=1;i<=n&&i<=m;i++)
+    {
+        if(n%i==0 && m%i==0)
+        {
+            s=i;
+        }
+    }
+    printf("%d",s);
 }
