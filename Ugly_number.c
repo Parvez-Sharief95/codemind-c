7df91eaaa2d0f62088ad2d793c2288c5ad702db3
Parvez-Sharief95 <119729990+Parvez-Sharief95@users.noreply.github.com>
2023-05-24 17:34:59 +0530

    # include <stdio.h> # include <string.h>
    int main()
    {
    int n,x=0;
    
      scanf("%d",&n);
    
          if(n<=0)
            {  
               
            }
           while(n!= 1) 
           {  
                if(n%5==0) 
                {  
                    n/=5;  
                } 
                else if(n%3==0) 
                {  
                    n/=3;  
                } 
                else if(n%2==0) 
                {  
                    n/=2;  
                } 
                else 
                {  
                    printf("Not Ugly Number"); 
                    x=1;  
                    break;  
                }  
            } 
    		        if(x==0)
    		        { 
                    printf("Ugly Number");
                    }
    }

--- Ugly_number.c
+++ Ugly_number.c
@@ -4,32 +4,35 @@
 int main()
 {
 int n,x=0;
+
   scanf("%d",&n);
 
-      if (n <= 0) {  
+      if(n<=0)
+        {  
+           
         }
-       while (n != 1) 
+       while(n!= 1) 
        {  
-            if (n % 5 == 0) 
+            if(n%5==0) 
             {  
-                n /= 5;  
+                n/=5;  
             } 
-            else if (n % 3 == 0) 
+            else if(n%3==0) 
             {  
-                n /= 3;  
+                n/=3;  
             } 
-            else if (n % 2 == 0) 
+            else if(n%2==0) 
             {  
-                n /= 2;  
+                n/=2;  
             } 
             else 
             {  
                 printf("Not Ugly Number"); 
-                x = 1;  
+                x=1;  
                 break;  
             }  
         } 
-		        if (x==0)
+		        if(x==0)
 		        { 
                 printf("Ugly Number");
                 }
